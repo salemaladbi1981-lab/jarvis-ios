6ac2e28ed93b573b6ca4d21320b1cbb30820f2c7
JARVIS <jarvis@salemai.studio>
2026-09-15 17:31:25 +0000

    Bug#3: add QuickCommand.swift to Xcode target (fix missing source)

--- generate_project.py
+++ generate_project.py
@@ -10,7 +10,7 @@ def uid():
 APP_SOURCES = [
     "JARVIS/App/JARVISApp.swift", "JARVIS/App/ContentView.swift",
     "JARVIS/Home/HomeView.swift", "JARVIS/Home/HomeViewModel.swift",
-    "JARVIS/Home/HeaderView.swift", "JARVIS/Home/QuickSuggestions.swift",
+    "JARVIS/Home/HeaderView.swift", "JARVIS/Home/QuickSuggestions.swift", "JARVIS/Home/QuickCommand.swift",
     "JARVIS/Home/VoiceInputBar.swift", "JARVIS/Home/BottomNavBar.swift",
     "JARVIS/Core/JarvisCoreView.swift", "JARVIS/Core/JarvisOrbitView.swift", "JARVIS/Core/WaveformView.swift",
     "JARVIS/Core/JarvisHeroView.swift", "JARVIS/Core/ApprovalCardView.swift",
