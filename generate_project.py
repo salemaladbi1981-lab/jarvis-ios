80f45c14b719d38a28f475739bfea14321f25d4e
JARVIS <jarvis@salemai.studio>
2026-09-16 18:18:54 +0000

    fix: remove orphan mac_dep_proxy + duplicate session_guard_bf (pbxproj validity)

--- generate_project.py
+++ generate_project.py
@@ -191,13 +191,10 @@ mac_test_debug = add("XCBuildConfiguration", buildSettings=dict(mac_test_setting
 mac_test_release = add("XCBuildConfiguration", buildSettings=dict(mac_test_settings), name="Release")
 mac_test_config_list = add("XCConfigurationList", buildConfigurations=[mac_test_debug, mac_test_release], defaultConfigurationIsVisible=0, defaultConfigurationName="Release")
 mac_test_bf = {p: add("PBXBuildFile", fileRef=test_refs[p]) for p in TEST_SOURCES}
-session_guard_bf = {p: add("PBXBuildFile", fileRef=app_src_refs[p]) for p in SESSION_GUARD_SOURCES}
-mac_test_sources_phase = add("PBXSourcesBuildPhase", files=sorted(mac_test_bf.values()), buildActionMask=2147483647, runOnlyForDeploymentPostprocessing=0)
-mac_dep_proxy = add("PBXContainerItemProxy", containerPortal=None, proxyType=1, remoteGlobalIDString=mac_target, remoteInfo="JARVIS Mac")
-mac_dep = add("PBXTargetDependency", target=mac_target, targetProxy=mac_dep_proxy)
+mac_test_sources_phase = add("PBXSourcesBuildPhase", files=sorted(list(mac_test_bf.values()) + list(session_guard_bf.values())), buildActionMask=2147483647, runOnlyForDeploymentPostprocessing=0)
 mac_test_target = add("PBXNativeTarget", buildConfigurationList=mac_test_config_list,
     buildPhases=[mac_test_sources_phase, test_frameworks_phase, test_resources_phase],
-    buildRules=[], dependencies=[mac_dep], name="JARVISTestsMac", productName="JARVISTestsMac",
+    buildRules=[], dependencies=[], name="JARVISTestsMac", productName="JARVISTestsMac",
     productReference=mac_test_prod_ref, productType="com.apple.product-type.bundle.unit-test")
 
 proj = add("PBXProject", attributes={"LastUpgradeCheck": "1500"},
